3c904bfd0bc450c0fa2914b05742fa8b9d10b97e
Chance Phiniezy <phiniezyc@gmail.com>
2017-11-07 04:57:43 -0500

    added 2nd cat pic w/ working click counter

--- catCounter.js
+++ catCounter.js
@@ -1,9 +1,15 @@
-let catPicClicks = 0;
+let cat1PicClicks = 0;
+let cat2PicClicks = 0;
 
 document.getElementById("catPic").addEventListener("click", function(){
-    catPicClicks++;
-    document.getElementById("catClicks").innerHTML = catPicClicks;
-    console.log(catPicClicks);
+    cat1PicClicks++;
+    document.getElementById("cat1Clicks").innerHTML = cat1PicClicks;
+    console.log(cat1PicClicks);
 });
 
+document.getElementById("catPic2").addEventListener("click", function(){
+    cat2PicClicks++;
+    document.getElementById("cat2Clicks").innerHTML = cat2PicClicks;
+    console.log(cat2PicClicks);
+});
 
